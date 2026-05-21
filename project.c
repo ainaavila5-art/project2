3b42a49d5a3845a67aa783745c1897a5a2a5eb43
Aina <aina.avila5@gmail.com>
2026-05-21 10:59:12 +0200

    preparation_and_environment_setup

--- project.c
+++ project.c
@@ -12,8 +12,9 @@ Students and NIUS:
 #include <stdlib.h>
 #include <string.h>
 
-//------------------------------------------------------------ Preparation and environment setup
-//DATE: 20/05/2026
+//------------------------------------------------------------ PREPARATION AND ENVIRONMENT SETUP
+
+//DATE: 20/05/2026. With the #ifdef directive we can dyamically load different civil registry datasets
 #define SMALL
 #ifdef SMALL
     #include "small.h"
@@ -25,26 +26,70 @@ Students and NIUS:
     #error "Incorrect definition. You should choose between these options: SMALL, MEDIUM LARGE"
 #endif
 
+/*DATE: 21/05/2026. This function checks if the input introduced by the user is correct
+by using a series of conditionals
+*/
+void CheckArguments (int argc, char **argv){
+    if (argc != 2){
+        printf("Error: Incorrect number of arguments.\n");
+        printf("Usage: %s", argv[0]);
+        exit(1);
+    }
+    if (strcmp(argv[1], "dfs") != 0 && strcmp(argv[1], "bfs") != 0) {
+        printf("Error: The argument must be strictly 'dfs' or 'bfs'. \n");
+        exit(1);
+    }
+}
 
-// Afegeix una ciutat
-struct RoadMap* addToRoadMap(struct RoadMap *head, int city_id, int total_cost);
+//DATE: 21/05/2026. This function adds every city chosen to the end of an array
+struct RoadMap* addToRoadMap(struct RoadMap *head, int city_id, int total_cost){
+    struct RoadMap *new_city = (struct RoadMap*)malloc(sizeof(struct RoadMap));
+    new_city -> city_id = city_id;
+    new_city -> total_cost = total_cost;
+    new_city -> next = NULL;
 
-// Imprimeix la llista
-void printRoadMap(struct RoadMap *head);
+    if (head == NULL){
+        return new_city;
+    }
+    struct RoadMap *current = head;
+    while (current -> next != NULL){
+        current = current -> next;
+    }
+    current -> next = new_city;
+    return head;
+}
 
-// Esborra tota la llista i retorna NULL perquè puguis actualitzar el punter original
-struct RoadMap* deleteAllRoadMap(struct RoadMap *head);
+//DATE: 21/05/2026. This function prints the list of the cities in the array
+void printRoadMap(struct RoadMap *head){
+    struct RoadMap *current = head;
+    while (current != NULL){
+        printf(" -> %s (Cost: %d)\n", citiesInfo[current->city_id].city_name, current->total_cost);
+        current = current -> next;
+    }
+}
+
+//DATE: 21/05/2026. This function deletes all the array containing the cities
+struct RoadMap *deleteAllRoadMap(struct RoadMap *head){
+    struct RoadMap *current = head;
+    struct RoadMap *temp;
+
+    while (current != NULL){
+        temp = current;
+        current = current -> next;
+        free(temp);
+    }
+    head = NULL;
+    return head;
+}
 
 
-
-
-//------------------------------------------------------------ Navigation and graph implementation
+//------------------------------------------------------------ NAVIGATION AND GRAPH IMPLEMENTATION
 // Cerca la ruta
 int RouteSearch(int source_city_id, int destination_city_id, struct RoadMap *road_map);
 
 
 
-//------------------------------------------------------------ Ancestor's tree construction
+//------------------------------------------------------------ ANCESTOR'S TREE CONSTRUCTION
 // Construeix l'arbre DFS
 struct TreeNode* createTreeDFS(int current_city_id, struct RoadMap *road_map);
 
@@ -59,7 +104,7 @@ void freeFamilyTree(struct TreeNode *root);
 
 
 
-//----------------------------------------------------------- Closing and definition
+//----------------------------------------------------------- CLOSING AND DEFINITION
 int main(int argc, char **argv); // Utilitzem l'argv per poder passar els paràmetres per la terminal
 
 
--- temporal_main.c
+++ temporal_main.c
@@ -1,9 +1,18 @@
 #include "project.c"
 
 int main(int argc, char **argv) {
-    printf("--- TEST ---\n");
-    printf("Number of cities: %d\n", NUMBER_CITIES);
-   
-    return 0;
+    CheckArguments(argc,argv);  
+
+    struct RoadMap *mytrip = NULL;
+    mytrip = addToRoadMap(mytrip, 0,0);
+    mytrip = addToRoadMap(mytrip, 1,150);
+    mytrip = addToRoadMap(mytrip, 2,280);
+
+    printRoadMap(mytrip);
+
+    mytrip = deleteAllRoadMap(mytrip);
+
+    printRoadMap(mytrip);
 }
+
 
